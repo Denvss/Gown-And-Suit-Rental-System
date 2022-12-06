9be0b31dcf7ee2633b30e442d9a245cb05e9f7ff
Rhenan A. Rosales <113603321+Enan09016@users.noreply.github.com>
2022-12-06 23:45:05 +0800

    Update Rentalmain.py

--- Rentalmain.py
+++ Rentalmain.py
@@ -18,13 +18,39 @@ while True:
     """)
 
     choice = input("Choose Yours :) : ")
-   
-print("testing")
-print ("testing")
 
-print("pogi")
-print ("eronz")
+    try:
+        if choice == "A" or choice == "a":
+            print("""
+                ||||| Gown Menu |||||
+                1. Display Available Gowns
+                2. Rent a Gown for a Day (₱800)
+                3. Rent a Gown for a Week (₱4800)
+                4. Return a Gown
+                5. Main Menu
+                6. Exit
+                """)
+            gSelect = int(input("Choose Yours:) : "))
 
-print("pasado sa python")
-print ("makakatapos sa project")
-print("makakatapos den tayo")
+            if gSelect == 1:
+                gown.displayStock()
+            elif gSelect == 2:
+                customer.rentalTimeG = gown.rentDay(customer.rentOutfit("gown"))
+                customer.rentalBasisG = 1
+                print("------------------------------------")
+            elif gSelect == 3:
+                customer.rentalTimeG = gown.rentWeek(customer.rentOutfit("gown"))
+                customer.rentalBasisG = 2
+                print("------------------------------------")
+            elif gSelect == 4:
+                customer.bill = gown.returnOutfit(customer.returnOutfit("gown"), "gown")
+                customer.rentalBasisG, customer.rentalTimeG, customer.gowns = 0, 0, 0
+                print("------------------------------------")
+            elif gSelect == 5:
+                print("||||| Welcome to JRRK Wardrove Rental Shop System |||||")
+            elif gSelect == 6:
+                print("Thank you for trusting JRRK Wardrove Rental Shop! Have a nice day!")
+
+                break
+            else:
+                print("Invalid Input! Please enter a number between 1 - 6")
