cd6161ae1db874d1554b74018a9c3d46e78ecef9
Denvss <21-07322@g.batstate-u.edu.ph>
2022-12-06 23:08:37 +0800

    Create Rentalmain.py

---
+++ Rentalmain.py
@@ -0,0 +1,20 @@
+from rent import Gown_Rent, Suit_Rent, Customer
+
+gown = Gown_Rent(100)
+suit = Suit_Rent(100)
+customer = Customer()
+
+def welcome():
+    print("||||| Welcome to JRRK Wardrove Rental Shop System |||||")
+    start = input("\nPress any key to continue...")
+
+welcome()
+
+while True:
+    print("""\n
+                    A. Gown Menu
+                    B. Suit Menu
+                    E. Exit
+    """)
+
+    choice = input("Choose Yours :) : ")
