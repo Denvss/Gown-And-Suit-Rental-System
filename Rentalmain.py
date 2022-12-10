c7b50435b0af3806b53254018fe3bf1af6a1516a
Rhenan A. Rosales <113603321+Enan09016@users.noreply.github.com>
2022-12-11 02:38:07 +0800

    Update Rentalmain.py

--- Rentalmain.py
+++ Rentalmain.py
@@ -77,3 +77,66 @@ def rental():
                     break
                 else:
                      print("Invalid Input! Please enter a number between 1 - 6")
+             
+            elif choice == "B" or choice == "b":
+                    print("""
+                        ||||| Suit Menu |||||
+                        1. Display Available Suits
+                        2. Rent a Suit for a Day (₱700)
+                        3. Rent a Suit for a Week (₱4200)
+                        4. Return a Suit
+                        5. Main Menu
+                        6. Exit
+                        
+                        """)
+                    sSelect = int(input("Choose Yours 🙂 : "))
+                    
+                    if sSelect == 1:
+                        Rent.Suit_Rent(100).displayStock()
+                    elif sSelect == 2:
+                        customer.rentalTimeS = Rent.Suit_Rent(100).rentDay(customer.rentOutfit("suit"))
+                        customer.rentalBasisS = 1
+                        print("------------------------------------")
+                    elif sSelect == 3:
+                        customer.rentalTimeS = Rent.Suit_Rent(100).rentWeek(customer.rentOutfit("suit"))
+                        customer.rentalBasisS = 2
+                        print("------------------------------------")
+                    elif sSelect == 4:
+                        customer.bill = Rent.Suit_Rent(100).returnOutfit(customer.returnOutfit("suit"), "suit")
+                        customer.rentalBasisS, customer.rentalTimeS, customer.suits = 0, 0, 0
+                        print("------------------------------------")
+                    elif sSelect == 5:
+                        print("Thank you for trusting JRRK Wardrove Rental Shop! Have a nice day!")
+                    elif sSelect == 6:
+                        break
+                    else:
+                        print("Invalid Input! Please enter a number between 1 - 6")
+                
+            elif choice == "E" or choice == "e":
+                print("Thank you for trusting JRRK Wardrove Rental Shop! Have a nice dayyyy!")
+                break
+            else:
+                print("Invalid Input! Please enter A, B or E!")
+
+        except ValueError:
+            print("Value Inputted is not a number!")
+            continue
+
+
+# START PROGRAM
+welcome()
+
+menu()
+
+ch = input("Enter your choice: ")
+
+cond = ""
+while (cond != "E" and cond != "e"):
+    if ch == "A" or ch == "a": 
+        customerManage()
+    elif ch == "B" or ch == "b":
+        rental()
+    elif ch == "E" or ch == "e":
+        print("\nThank You Very Much!")
+    else:
+        print("\nInvalid Input")
